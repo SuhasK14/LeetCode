f9cfc423927f7ae77bfcedd7c668978a3be39d78
SuhasK14 <113224131+SuhasK14@users.noreply.github.com>
2024-01-04 19:22:13 -0600

    Update 1207. Unique Number of Occurrences.java

--- 1207. Unique Number of Occurrences.java
+++ 1207. Unique Number of Occurrences.java
@@ -2,6 +2,24 @@ class Solution
 {
     public boolean uniqueOccurrences(int[] arr)
     {
-        
+        // create a map to count the number of occurrences of each value in arr
+        HashMap<Integer, Integer> occurrences = new HashMap<>();
+        // loop through arr and map all
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (occurrences.containsKey(arr[i]))
+            {
+                int temp = occurrences.get(arr[i]);
+                occurrences.put(arr[i], ++temp);
+            }
+            else
+            {
+                occurrences.put(arr[i], 1);
+            }
+        }
+        // create a hashset of the values in hashmap occurrences
+        HashSet<Integer> values = new HashSet<>(occurrences.values());
+        // return true or false based on if the sizes match
+        return occurrences.size() == values.size();
     }
 }
