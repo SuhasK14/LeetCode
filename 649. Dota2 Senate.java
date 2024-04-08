83287a1d187d77f6bd9b7bb2b15aa165713c8de7
Suhas Kurukuri <113224131+SuhasK14@users.noreply.github.com>
2024-04-07 22:36:50 -0500

    Create 649. Dota2 Senate.java

---
+++ 649. Dota2 Senate.java
@@ -0,0 +1,40 @@
+class Solution
+{
+    public String predictPartyVictory(String senate)
+    {
+        Queue<Integer> radiant = new LinkedList<>();
+        Queue<Integer> dire = new LinkedList<>();
+        for (int i = 0; i < senate.length(); i++)
+        {
+            if (senate.charAt(i) == 'R')
+            {
+                radiant.add(i);
+            }
+            else
+            {
+                dire.add(i);
+            }
+        }
+        while (!radiant.isEmpty() && !dire.isEmpty())
+        {
+            int r = radiant.poll();
+            int d = dire.poll();
+            if (r < d)
+            {
+                radiant.add(r + senate.length());
+            }
+            else
+            {
+                dire.add(d + senate.length());
+            }
+        }
+        if (radiant.size() > dire.size())
+        {
+            return "Radiant";
+        }
+        else 
+        {
+            return "Dire";
+        }
+    }
+}
