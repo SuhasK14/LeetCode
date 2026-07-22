4a21f7d9a6e297ac153b19ed18fcaedad34c9bff
Suhas Kurukuri <113224131+SuhasK14@users.noreply.github.com>
2026-07-22 13:20:46 -0500

    Update 11. Container With Most Water.java

--- 11. Container With Most Water.java
+++ 11. Container With Most Water.java
@@ -1,20 +1,17 @@
-class Solution
-{
-    public int maxArea(int[] height)
-    {
+class Solution {
+    public int maxArea(int[] height) {
         int max = 0;
+        // two pointer approach, starting from ends
         int left = 0;
-        int right = height.length -1;
-        while (left < right)
-        {
+        int right = height.length - 1;
+        while (left < right) {
+            // calculate new max using min of both ends multiplied by distance between
             max = Math.max(max, Math.min(height[left], height[right]) * (right - left));
-            if (height[left] < height[right])
-            {
-                left++;
-            }
-            else
-            {
+            // move pointers based on which end is greater: move the lower one
+            if (height[left] > height[right]) {
                 right--;
+            } else {
+                left++;
             }
         }
         return max;
