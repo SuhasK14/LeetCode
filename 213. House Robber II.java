be7072f752f05335035950d3946f9d5ed7fa82b6
Suhas Kurukuri <113224131+SuhasK14@users.noreply.github.com>
2026-08-06 17:45:19 -0500

    Update 213. House Robber II.java

--- 213. House Robber II.java
+++ 213. House Robber II.java
@@ -1,36 +1,40 @@
 class Solution {
     public int rob(int[] nums) {
+        // check base cases
         if (nums.length == 1) {
             return nums[0];
-        }
-        if (nums.length == 2) {
+        } else if (nums.length == 2) {
             return Math.max(nums[0], nums[1]);
+        } else if (nums.length == 3) {
+            return Math.max(nums[0], Math.max(nums[1], nums[2]));
         }
-        if (nums.length == 3) {
-            return Math.max(Math.max(nums[0], nums[1]), nums[2]);
-        }
-        // Run for all houses 1 to N-1
-        int a = nums[0];
-        int b = nums[1];
+        // (excluding last element)
+        // DP array: set first 2 elements, prior base cases ensure valid inputs
+        int[] dp = new int[2];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        // cut off last element (nums.length - 1)
         for (int i = 2; i < nums.length - 1; i++) {
-            int temp = b;
-            // either skip this house (keep b) or rob it (nums[i](current) + 2 back(stored in a))
-            b = Math.max(b, (a + nums[i]));
-            // a keeps current value or switches to b as we go to next index forward
-            a = Math.max(a, temp);
+            // calculate dp by getting max of prior index (1) vs 2 prior index (0) + current nums[i]
+            int temp = Math.max(dp[1], dp[0] + nums[i]);
+            // move forward: 0 becomes 1, and 1 becomes new max
+            dp[0] = dp[1];
+            dp[1] = temp;
         }
-
-        // Run for all houses 2 to N
-        int c = nums[1];
-        int d = nums[2];
+        int first = dp[1]; // store max
+        // (excluding first element)
+        // DP array: set first 2 elements, prior base cases ensure valid inputs
+        dp = new int[2];
+        dp[0] = nums[1];
+        dp[1] = Math.max(nums[1], nums[2]);
+        // cut off first element, start at i = 3
         for (int i = 3; i < nums.length; i++) {
-            int temp = d;
-            // either skip this house (keep d) or rob it (nums[i](current) + 2 back(stored in c))
-            d = Math.max(d, (c + nums[i]));
-            // c keeps current value or switches to d as we go to next index forward
-            c = Math.max(c, temp);
+            // calculate dp by getting max of prior index (1) vs 2 prior index (0) + current nums[i]
+            int temp = Math.max(dp[1], dp[0] + nums[i]);
+            // move forward: 0 becomes 1, and 1 becomes new max
+            dp[0] = dp[1];
+            dp[1] = temp;
         }
-        // b and d holds the max profit for both approaches, so return max
-        return Math.max(b,d);
+        return Math.max(first, dp[1]);
     }
 }
