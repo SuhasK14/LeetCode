0988734c54926543d35d5542fa9e47e2fd6dbbf8
Suhas Kurukuri <113224131+SuhasK14@users.noreply.github.com>
2026-08-06 17:08:45 -0500

    Update 198. House Robber.java

--- 198. House Robber.java
+++ 198. House Robber.java
@@ -1,22 +1,22 @@
 class Solution {
     public int rob(int[] nums) {
+        // check base cases
         if (nums.length == 1) {
             return nums[0];
-        }
-        if (nums.length == 2) {
+        } else if (nums.length == 2) {
             return Math.max(nums[0], nums[1]);
         }
-        // Like Fibonacci but little different DP problem
-        int a = nums[0];
-        int b = nums[1];
+        // do array that contains max value up to that index (no looking ahead)
+        int[] dp = new int[nums.length];
+        // set first 2 elements
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        // loop through nums
         for (int i = 2; i < nums.length; i++) {
-            int temp = b;
-            // either skip this house (keep b) or rob it (nums[i](current) + 2 back(stored in a))
-            b = Math.max(b, (a + nums[i]));
-            // a keeps current value or switches to b as we go to next index forward
-            a = Math.max(a, temp);
+            // calculate dp by getting max of prior index vs current index + 2 indexes back
+            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);
         }
-        // b holds the max profit between a + cost and b, so return
-        return b;
+        // last index contains maximum amount
+        return dp[nums.length - 1];
     }
 }
