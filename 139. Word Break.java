9d66c0ddcdcbb6fd6fd5ba2ea86c2f9428115e89
Suhas Kurukuri <113224131+SuhasK14@users.noreply.github.com>
2026-08-11 23:19:57 -0500

    Update 139. Word Break.java

--- 139. Word Break.java
+++ 139. Word Break.java
@@ -5,33 +5,32 @@ class Solution {
         for (String word : wordDict) {
             set.add(word);
         }
-        // use hashmap to get already traversed values, prevent time limit exceeded exception
-        HashMap<Integer, Boolean> map = new HashMap<>();
-        return canBreak(set, s, 0, map);
+        // use string array to store traversed values (prevents time limit exceeded exception)
+        Boolean[] visited = new Boolean[s.length()];
+        return canBreak(set, s, 0, visited);
     }
-    private boolean canBreak(Set<String> set, String s, int start, HashMap<Integer, Boolean> map) {
+    private boolean canBreak(Set<String> set, String s, int start, Boolean[] visited) {
         // reached end of string
         if (start == s.length()) {
             return true;
         }
-        // check if already traversed and return boolean stored in map
-        if (map.containsKey(start)) {
-            return map.get(start);
+        // check if already traversed and return boolean stored at index start
+        if (visited[start] != null) {
+            return visited[start];
         }
-        // use temp and loop through s until word is found in set, from there, call canBreak and continue appending to current
-        String temp = "";
+        /* loop through s until substring is found in word set, then call canBreak using new start
+        (continue appending to current substring in case of further longer matches...) */
         for (int i = start; i < s.length(); i++) {
-            temp += s.charAt(i);
-            if (set.contains(temp)) {
-                if (canBreak(set, s, i + 1, map)) {
-                    // store in map and return true
-                    map.put(i, true);
+            if (set.contains(s.substring(start, i + 1))) {
+                if (canBreak(set, s, i + 1, visited)) {
+                    // set true in boolean array and return true
+                    visited[start] = true;
                     return true;
                 }
             }
         }
-        // store in map and return false
-        map.put(start, false);
+        // set false in boolean array and return false
+        visited[start] = false;
         return false;
     }
 }
