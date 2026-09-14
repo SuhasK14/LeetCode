3f89c618138deb1936abb2f340068c699d97813a
Suhas Kurukuri <113224131+SuhasK14@users.noreply.github.com>
2026-09-14 14:28:25 -0500

    Update 290. Word Pattern.java

--- 290. Word Pattern.java
+++ 290. Word Pattern.java
@@ -1,25 +1,26 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
         // maps to check both ways
-        HashMap<String, String> mapLW = new HashMap<>();
-        HashMap<String, String> mapWL = new HashMap<>();
+        HashMap<Character, String> mapLW = new HashMap<>();
+        HashMap<String, Character> mapWL = new HashMap<>();
         String[] words = s.split(" ");
         // check to make sure number of words = number of letter, otherwise return false
         if (pattern.length() != words.length) {
             return false;
         }
-        // loop
         for (int i = 0; i < words.length; i++) {
+            char letter = pattern.charAt(i);
+            String word = words[i];
             // conditionals: check if letter maps to the appropriate word and if word maps to the appropriate letter
-            if (mapLW.containsKey(pattern.substring(i, i+1)) && !mapLW.get(pattern.substring(i, i+1)).equals(words[i])) {
+            if (mapLW.containsKey(letter) && !mapLW.get(letter).equals(word)) {
                 return false;
             }
-            if (mapWL.containsKey(words[i]) && !mapWL.get(words[i]).equals(pattern.substring(i, i+1))) {
+            if (mapWL.containsKey(word) && !mapWL.get(word).equals(letter)) {
                 return false;
             }
             // map both ways: LW, WL
-            mapLW.put(pattern.substring(i, i+1), words[i]);
-            mapWL.put(words[i], pattern.substring(i, i+1));
+            mapLW.put(letter, word);
+            mapWL.put(word, letter);
         }
         return true;
     }
